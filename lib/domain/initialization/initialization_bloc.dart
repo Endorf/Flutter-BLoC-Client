719a3682ff89611bf2df498620c980eca6bcb3d5
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-09 20:17:48 +0300

    create init bloc

---
+++ lib/domain/initialization/initialization_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_app/data/fake_repository.dart';
+import 'package:bloc_app/domain/initialization/event.dart';
+import 'package:bloc_app/domain/initialization/state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class InitializationBloc extends Bloc<Event, SplashState> {
+  // TODO: use DI
+  final repository = FakeRepository();
+
+  InitializationBloc() : super(SplashState()) {
+    on<Event>((event, emit) async {
+      emit(SplashState(isLoading: true));
+
+      try {
+        await repository.authenticate();
+
+        emit(SplashState(isReady: true, isAuthenticated: true));
+      } catch (error) {
+        emit(SplashState(isReady: true));
+      }
+    });
+  }
+}
