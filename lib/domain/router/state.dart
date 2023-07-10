66185bbbf1a2a6f25ba6a4dc880a22b7b559a002
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-10 18:07:41 +0300

    remove deprecated state

--- lib/domain/router/state.dart
+++ lib/domain/router/state.dart
@@ -1,15 +1,3 @@
-class RouterState1 {
-  final bool isLoading;
-  final bool isReady;
-  final bool isAuthenticated;
-
-  RouterState1({
-    this.isLoading = false,
-    this.isReady = false,
-    this.isAuthenticated = false,
-  });
-}
-
 abstract class RouterState {}
 
 class SplashState implements RouterState {}
