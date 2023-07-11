d6f4622c5f7a6614fa9ba4fdc264e38503ce4410
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-11 17:56:34 +0300

    update creation page

---
+++ lib/ui/screen/creation/creation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreationScreen extends StatelessWidget {
+  const CreationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Creation screen",
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
---
+++ lib/ui/screen/creation/page/creation_page.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_app/ui/screen/base_page.dart';
+import 'package:bloc_app/ui/screen/creation/creation.dart';
+import 'package:flutter/material.dart';
+
+class CreationPage extends BasePage {
+  @override
+  Widget get screen => const CreationScreen();
+}
