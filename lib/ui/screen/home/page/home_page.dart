ec8ebb3f370aa4d25b128bef138135279457ca16
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-11 17:56:26 +0300

    update home page

--- lib/ui/screen/home/page/home_page.dart
+++ lib/ui/screen/home/page/home_page.dart
@@ -1,19 +1,8 @@
+import 'package:bloc_app/ui/screen/base_page.dart';
 import 'package:bloc_app/ui/screen/home/home.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends Page {
+class HomePage extends BasePage {
   @override
-  Route createRoute(BuildContext context) {
-    return PageRouteBuilder(
-      settings: this,
-      pageBuilder: (context, animation, animation2) {
-        final tween = Tween(begin: const Offset(0.0, 1.0), end: Offset.zero);
-        final curveTween = CurveTween(curve: Curves.easeInOut);
-        return SlideTransition(
-          position: animation.drive(curveTween).drive(tween),
-          child: const HomeScreen(),
-        );
-      },
-    );
-  }
+  Widget get screen => const HomeScreen();
 }
