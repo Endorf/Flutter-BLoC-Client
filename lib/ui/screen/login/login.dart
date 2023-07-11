736705f268c711a584df49af23639b938281e60d
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-11 17:56:17 +0300

    update login page

--- lib/ui/screen/login/login.dart
+++ lib/ui/screen/login/login.dart
@@ -1,15 +1,31 @@
+import 'package:bloc_app/domain/router/event.dart';
+import 'package:bloc_app/domain/router/router_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Login screen",
-        textAlign: TextAlign.center,
-      ),
+    return Column(
+      children: [
+        const Center(
+          child: Text(
+            "Login screen",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            textStyle: const TextStyle(fontSize: 20),
+          ),
+          onPressed: () => context.read<RouterBloc>().add(HomeEvent()),
+          child: const Text('Login'),
+        ),
+      ],
     );
   }
 }
--- lib/ui/screen/login/page/login_page.dart
+++ lib/ui/screen/login/page/login_page.dart
@@ -1,19 +1,8 @@
+import 'package:bloc_app/ui/screen/base_page.dart';
 import 'package:bloc_app/ui/screen/login/login.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends Page {
+class LoginPage extends BasePage {
   @override
-  Route createRoute(BuildContext context) {
-    return PageRouteBuilder(
-      settings: this,
-      pageBuilder: (context, animation, animation2) {
-        final tween = Tween(begin: const Offset(0.0, 1.0), end: Offset.zero);
-        final curveTween = CurveTween(curve: Curves.easeInOut);
-        return SlideTransition(
-          position: animation.drive(curveTween).drive(tween),
-          child: const LoginScreen(),
-        );
-      },
-    );
-  }
+  Widget get screen => const LoginScreen();
 }
