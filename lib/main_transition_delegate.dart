ee2aab1b4913f7b94a0fbc1d61d8997caa10d759
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-12 13:53:56 +0300

    add TransitionDelegate

---
+++ lib/main_transition_delegate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MainTransitionDelegate extends TransitionDelegate<void> {
+  @override
+  Iterable<RouteTransitionRecord> resolve(
+      {required List<RouteTransitionRecord> newPageRouteHistory,
+      required Map<RouteTransitionRecord?, RouteTransitionRecord>
+          locationToExitingPageRoute,
+      required Map<RouteTransitionRecord?, List<RouteTransitionRecord>>
+          pageRouteToPagelessRoutes}) {
+    final List<RouteTransitionRecord> results = <RouteTransitionRecord>[];
+
+    for (final RouteTransitionRecord pageRoute in newPageRouteHistory) {
+      if (pageRoute.isWaitingForEnteringDecision) {
+        pageRoute.markForPush();
+      }
+      results.add(pageRoute);
+    }
+    for (final RouteTransitionRecord exitingPageRoute
+        in locationToExitingPageRoute.values) {
+      exitingPageRoute.markForPop();
+      final List<RouteTransitionRecord>? pagelessRoutes =
+          pageRouteToPagelessRoutes[exitingPageRoute];
+      if (pagelessRoutes != null) {
+        for (final RouteTransitionRecord pagelessRoute in pagelessRoutes) {
+          pagelessRoute.markForRemove();
+        }
+      }
+      results.add(exitingPageRoute);
+    }
+    return results;
+  }
+}
