5f3715f22dbdbc948c07246de49b5924c4ea3973
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-13 22:43:34 +0300

    update app themes

--- lib/application.dart
+++ lib/application.dart
@@ -25,8 +25,8 @@ class Application extends StatelessWidget {
       ],
       child: MaterialApp(
         title: Strings.appName,
-        // TODO: add check device dark\light mode. Reuse ThemeConfig[light|dark]
-        theme: _themeLightData(ThemeData.light()),
+        theme: ThemeConfig.themeData(ThemeData.light()),
+        darkTheme: ThemeConfig.themeData(ThemeData.dark()),
         home: _routers,
       ),
     );
@@ -41,66 +41,4 @@ class Application extends StatelessWidget {
           backButtonDispatcher: RootBackButtonDispatcher(),
         ),
       );
-
-  ThemeData _themeLightData(ThemeData theme) {
-    return theme.copyWith(
-      colorScheme: theme.colorScheme.copyWith(
-        primary: ThemeConfig.experimentalAccentColor,
-        // secondary: ThemeConfig.experimentalAccentColor,
-        // onPrimary: ThemeConfig.experimentalAccentColor,
-        // primaryContainer: ThemeConfig.experimentalAccentColor,
-        // onPrimaryContainer: ThemeConfig.experimentalAccentColor,
-        // onSecondary: ThemeConfig.experimentalAccentColor,
-        // secondaryContainer: ThemeConfig.experimentalAccentColor,
-        // onSecondaryContainer: ThemeConfig.experimentalAccentColor,
-        // tertiary: ThemeConfig.experimentalAccentColor,
-        // onTertiary: ThemeConfig.experimentalAccentColor,
-        // tertiaryContainer: ThemeConfig.experimentalAccentColor,
-        // onTertiaryContainer: ThemeConfig.experimentalAccentColor,
-        // required this.error,
-        // required this.onError,
-        // Color? errorContainer,
-        // Color? onErrorContainer,
-        background: ThemeConfig.experimentalBackgroundColor,
-        // onBackground: ThemeConfig.experimentalAccentColor,
-        // surface: ThemeConfig.experimentalAccentColor,
-        // onSurface: ThemeConfig.experimentalAccentColor,
-        // surfaceVariant: ThemeConfig.experimentalAccentColor,
-        onSurfaceVariant: ThemeConfig.experimentalTextColor,
-        // outline: ThemeConfig.experimentalAccentColor,
-        // outlineVariant: ThemeConfig.experimentalAccentColor,
-        // shadow: ThemeConfig.experimentalAccentColor,
-        // scrim: ThemeConfig.experimentalAccentColor,
-        // inverseSurface: ThemeConfig.experimentalAccentColor,
-        // onInverseSurface: ThemeConfig.experimentalAccentColor,
-        // inversePrimary: ThemeConfig.experimentalAccentColor,
-        // surfaceTint: ThemeConfig.experimentalAccentColor,
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: ThemeConfig.experimentalAccentColor,
-            foregroundColor: ThemeConfig.experimentalTertiary,
-            elevation: 1,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            )),
-      ),
-      textTheme: const TextTheme(
-        displayMedium: TextStyle(color: ThemeConfig.experimentalTextColor),
-        displaySmall: TextStyle(color: ThemeConfig.experimentalTextColor),
-        headlineMedium: TextStyle(color: ThemeConfig.experimentalTextColor),
-        headlineSmall: TextStyle(color: ThemeConfig.experimentalTextColor),
-        titleLarge: TextStyle(color: ThemeConfig.experimentalTextColor),
-        titleMedium: TextStyle(color: ThemeConfig.experimentalTextColor),
-        titleSmall: TextStyle(color: ThemeConfig.experimentalTextColor),
-        bodyLarge: TextStyle(color: ThemeConfig.experimentalTextColor),
-        bodyMedium: TextStyle(color: ThemeConfig.experimentalTextColor),
-        bodySmall: TextStyle(color: ThemeConfig.experimentalTextColor),
-        labelLarge: TextStyle(color: ThemeConfig.experimentalAccentColor),
-        labelMedium: TextStyle(color: ThemeConfig.experimentalAccentColor),
-        labelSmall: TextStyle(color: ThemeConfig.experimentalAccentColor),
-      ),
-      useMaterial3: true,
-    );
-  }
 }
