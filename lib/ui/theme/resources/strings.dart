b61e661e707040266966da0d560fb93d040c746b
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-14 15:11:35 +0300

    add titles to res

--- lib/ui/theme/resources/strings.dart
+++ lib/ui/theme/resources/strings.dart
@@ -2,6 +2,8 @@ class Strings {
   static const appName = 'Flutter Demo';
 
   static const loginTitle = 'Authorization';
+  static const homeTitle = 'Home';
+  static const creationTitle = 'Creation';
 
   static const emailLabel = 'Email';
 
