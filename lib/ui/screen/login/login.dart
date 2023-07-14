f9b4e11f75477eacd6d7812ba5957d921af76521
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-14 14:11:18 +0300

    transit to home screen

--- lib/ui/screen/login/login.dart
+++ lib/ui/screen/login/login.dart
@@ -1,8 +1,11 @@
+import 'package:bloc_app/domain/router/event.dart';
+import 'package:bloc_app/domain/router/router_bloc.dart';
 import 'package:bloc_app/ui/theme/material_theme_data.dart';
 import 'package:bloc_app/ui/theme/resources/strings.dart';
 import 'package:bloc_app/ui/widget/button.dart';
 import 'package:bloc_app/ui/widget/text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -81,7 +84,8 @@ class LoginScreenState extends State<LoginScreen> {
                               right: _buttonPaddingRight),
                           child: DefaultElevationButton(
                             labelText: Strings.enterTitle,
-                            onPressed: () => {},
+                            onPressed: () =>
+                                context.read<RouterBloc>().add(HomeEvent()),
                           )),
                     ],
                   ),
