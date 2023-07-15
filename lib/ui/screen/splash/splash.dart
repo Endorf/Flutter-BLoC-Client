27fdf768e2ed9a2d33491c2cb64c1a7e995f7a36
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 16:31:25 +0300

    remove comment

--- lib/ui/screen/splash/splash.dart
+++ lib/ui/screen/splash/splash.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashScreen extends StatelessWidget {
-  // const SplashScreen({super.key, required Null Function(dynamic index) onInitialization});
-
   final void Function(int) onInitialization;
 
   const SplashScreen({super.key, required this.onInitialization});
