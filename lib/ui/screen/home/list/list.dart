6f590cd4420699178972ed4b6fe6ee3287da3a08
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 16:32:09 +0300

    create ListScreen

--- lib/ui/screen/home/list/list.dart
+++ lib/ui/screen/home/list/list.dart
@@ -1,33 +1,11 @@
-import 'package:bloc_app/ui/screen/home/widget/note_list.dart';
-import 'package:bloc_app/ui/theme/resources/strings.dart';
-import 'package:bloc_app/ui/widget/app_container.dart';
+import 'package:bloc_app/ui/screen/home/list/widget/note_list.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  // const ListPage({super.key});
-
-  final void Function() onNewNote;
-
-  const HomeScreen({super.key, required this.onNewNote});
-  // const ListPage() : super(Pages.home, const ValueKey('home'));
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: _buildfloatingActionButton(context),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      body: const RootContentContainer(child: NoteList()),
-    );
-  }
-
-  Widget _buildfloatingActionButton(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-        child: FloatingActionButton(
-          backgroundColor: Theme.of(context).colorScheme.onTertiary,
-          tooltip: Strings.homeTooltip,
-          child: const Icon(Icons.note_alt_rounded),
-          onPressed: () => {onNewNote()},
-        ));
+    return const NoteList();
   }
 }
