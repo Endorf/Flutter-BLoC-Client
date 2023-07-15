92f3faceece941eae5ae2422e4a9139ea438f54a
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 16:35:05 +0300

    Go navigation (#9)

--- lib/ui/screen/home/home.dart
+++ lib/ui/screen/home/home.dart
@@ -1,28 +1,38 @@
-import 'package:bloc_app/domain/router/event.dart';
-import 'package:bloc_app/domain/router/router_bloc.dart';
-import 'package:bloc_app/ui/screen/base_screen.dart';
-import 'package:bloc_app/ui/screen/home/widget/note_list.dart';
 import 'package:bloc_app/ui/theme/resources/strings.dart';
+import 'package:bloc_app/ui/widget/app_container.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
-class HomeScreen extends BaseScreen {
-  const HomeScreen({super.key}) : super(title: Strings.homeTitle);
+class RootScreen extends StatelessWidget {
+  const RootScreen({
+    super.key,
+    required this.child,
+    required this.onFabClick,
+  });
+
+  final Widget child;
+  final void Function() onFabClick;
 
   @override
-  Widget buildChildren(BuildContext context) {
-    return const NoteList();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('App Shell')),
+      floatingActionButton: _buildfloatingActionButton(context),
+      body: RootContentContainer(child: child),
+    );
   }
 
-  @override
-  Widget? buildfloatingActionButton(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-        child: FloatingActionButton(
-          backgroundColor: Theme.of(context).colorScheme.onTertiary,
-          tooltip: Strings.homeTooltip,
-          child: const Icon(Icons.note_alt_rounded),
-          onPressed: () => context.read<RouterBloc>().add(CreationEvent()),
-        ));
+  Widget? _buildfloatingActionButton(BuildContext context) {
+    return GoRouter.of(context).routeInformationProvider.value.location !=
+            "/home/creation"
+        ? Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+            child: FloatingActionButton(
+              backgroundColor: Theme.of(context).colorScheme.onTertiary,
+              tooltip: Strings.homeTooltip,
+              child: const Icon(Icons.note_alt_rounded),
+              onPressed: () => {onFabClick()},
+            ))
+        : null;
   }
 }
---
+++ lib/ui/screen/home/list/list.dart
@@ -0,0 +1,11 @@
+import 'package:bloc_app/ui/screen/home/list/widget/note_list.dart';
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const NoteList();
+  }
+}
