c53d3ed56ecdfc2e39b44336e7d61f65a24a810f
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 15:24:25 +0300

    update home screen

---
+++ lib/ui/screen/home/creation/creation.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CreationScreen extends StatelessWidget {
+  final void Function() onCreate;
+
+  const CreationScreen({super.key, required this.onCreate});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Center(
+          child: Text(
+            "Creation screen",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        TextButton(
+          onPressed: () => onCreate,
+          child: const Text('Create'),
+        ),
+      ],
+    );
+  }
+}
--- lib/ui/screen/home/home.dart
+++ lib/ui/screen/home/home.dart
@@ -1,28 +1,30 @@
-import 'package:bloc_app/domain/router/event.dart';
-import 'package:bloc_app/domain/router/router_bloc.dart';
-import 'package:bloc_app/ui/screen/base_screen.dart';
-import 'package:bloc_app/ui/screen/home/widget/note_list.dart';
-import 'package:bloc_app/ui/theme/resources/strings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
-class HomeScreen extends BaseScreen {
-  const HomeScreen({super.key}) : super(title: Strings.homeTitle);
+class RootScreen extends StatelessWidget {
+  const RootScreen({
+    super.key,
+    required this.child,
+    required this.onTap,
+  });
+
+  final Widget child;
+  final void Function(int) onTap;
 
   @override
-  Widget buildChildren(BuildContext context) {
-    return const NoteList();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('App Shell')),
+      body: Center(
+        child: child,
+      ),
+    );
   }
 
-  @override
-  Widget? buildfloatingActionButton(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-        child: FloatingActionButton(
-          backgroundColor: Theme.of(context).colorScheme.onTertiary,
-          tooltip: Strings.homeTooltip,
-          child: const Icon(Icons.note_alt_rounded),
-          onPressed: () => context.read<RouterBloc>().add(CreationEvent()),
-        ));
+  int _getSelectedIndex(String location) {
+    if (location.startsWith('/home')) return 0;
+    if (location.startsWith('/creation')) return 1;
+    return 0;
   }
 }
