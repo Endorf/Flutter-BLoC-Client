3d8a42d155fbda76a0cfa39cc6b3cbae39348287
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 15:24:02 +0300

    update splash screen

--- lib/ui/screen/splash/splash.dart
+++ lib/ui/screen/splash/splash.dart
@@ -1,25 +1,25 @@
 import 'package:bloc_app/domain/initialization/initialization_bloc.dart';
 import 'package:bloc_app/domain/initialization/state.dart';
-import 'package:bloc_app/domain/router/event.dart';
-import 'package:bloc_app/domain/router/router_bloc.dart';
 import 'package:bloc_app/ui/widget/app_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  // const SplashScreen({super.key, required Null Function(dynamic index) onInitialization});
+
+  final void Function(int) onInitialization;
+
+  const SplashScreen({super.key, required this.onInitialization});
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<InitializationBloc, SplashState>(
         listener: (context, state) {
           if (state.isReady) {
-            final router = context.read<RouterBloc>();
-
             if (state.isAuthenticated) {
-              router.add(HomeEvent());
+              onInitialization(0);
             } else {
-              router.add(LoginEvent());
+              onInitialization(1);
             }
           }
         },
