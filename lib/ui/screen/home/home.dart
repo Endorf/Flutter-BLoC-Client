05fa1596581a92c6fe8a5bc529d14a23e1f1e124
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 16:32:36 +0300

    create root home screen

--- lib/ui/screen/home/home.dart
+++ lib/ui/screen/home/home.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:bloc_app/ui/theme/resources/strings.dart';
+import 'package:bloc_app/ui/widget/app_container.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,25 +7,32 @@ class RootScreen extends StatelessWidget {
   const RootScreen({
     super.key,
     required this.child,
-    required this.onTap,
+    required this.onFabClick,
   });
 
   final Widget child;
-  final void Function(int) onTap;
+  final void Function() onFabClick;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('App Shell')),
-      body: Center(
-        child: child,
-      ),
+      appBar: AppBar(title: const Text('App Shell')),
+      floatingActionButton: _buildfloatingActionButton(context),
+      body: RootContentContainer(child: child),
     );
   }
 
-  int _getSelectedIndex(String location) {
-    if (location.startsWith('/home')) return 0;
-    if (location.startsWith('/creation')) return 1;
-    return 0;
+  Widget? _buildfloatingActionButton(BuildContext context) {
+    return GoRouter.of(context).routeInformationProvider.value.location !=
+            "/home/creation"
+        ? Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+            child: FloatingActionButton(
+              backgroundColor: Theme.of(context).colorScheme.onTertiary,
+              tooltip: Strings.homeTooltip,
+              child: const Icon(Icons.note_alt_rounded),
+              onPressed: () => {onFabClick()},
+            ))
+        : null;
   }
 }
