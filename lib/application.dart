03b033e2f4e99a77519792b31a2747a568fdaf62
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-15 16:32:43 +0300

    update application

--- lib/application.dart
+++ lib/application.dart
@@ -1,44 +1,128 @@
 import 'package:bloc_app/domain/initialization/event.dart';
 import 'package:bloc_app/domain/initialization/initialization_bloc.dart';
-import 'package:bloc_app/domain/router/router_bloc.dart';
-import 'package:bloc_app/domain/router/state.dart';
-import 'package:bloc_app/ui/navigation/router_delegate.dart';
+import 'package:bloc_app/ui/screen/home/home.dart';
+import 'package:bloc_app/ui/screen/home/creation/creation.dart';
+import 'package:bloc_app/ui/screen/home/list/list.dart';
+import 'package:bloc_app/ui/screen/login/login.dart';
+import 'package:bloc_app/ui/screen/splash/splash.dart';
 import 'package:bloc_app/ui/theme/material_theme_data.dart';
-import 'package:bloc_app/ui/theme/resources/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class Application extends StatelessWidget {
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  final _rootNavigatorKey = GlobalKey<NavigatorState>();
+  final _shellNavigatorKey = GlobalKey<NavigatorState>();
 
   Application({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          lazy: false,
-          create: (context) => InitializationBloc()..add(Event()),
-        ),
-        BlocProvider(create: (context) => RouterBloc())
-      ],
-      child: MaterialApp(
-        title: Strings.appName,
-        theme: ThemeConfig.themeData(ThemeData.light()),
-        darkTheme: ThemeConfig.themeData(ThemeData.dark()),
-        home: _routers,
-      ),
+        providers: [
+          BlocProvider(
+            lazy: false,
+            create: (context) => InitializationBloc()..add(Event()),
+          ),
+        ],
+        child: MaterialApp.router(
+          theme: ThemeConfig.themeData(ThemeData.light()),
+          darkTheme: ThemeConfig.themeData(ThemeData.dark()),
+          debugShowCheckedModeBanner: true,
+          routerConfig: GoRouter(
+            navigatorKey: _rootNavigatorKey,
+            initialLocation: '/splash',
+            redirect: (context, state) {
+              if (state.path == '/') {
+                return '/splash';
+              }
+              return null;
+            },
+            debugLogDiagnostics: true,
+            routes: [
+              _buildSplashRoute(),
+              _buildLoginRoute(),
+              _buildHomeRoute(),
+            ],
+          ),
+        ));
+  }
+
+  GoRoute _buildSplashRoute() {
+    return GoRoute(
+      path: '/splash',
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          restorationId: 'route.splash',
+          fullscreenDialog: true,
+          child: SplashScreen(onInitialization: (index) {
+            if (index == 0) {
+              context.go('/home');
+            } else {
+              context.go('/login');
+            }
+          }),
+        );
+      },
     );
   }
 
-  Widget get _routers => BlocBuilder<RouterBloc, RouteState>(
-        builder: (context, state) => Router(
-          routerDelegate: RootRouterDelegate(
-            navigatorKey,
-            context.read<RouterBloc>(),
+  GoRoute _buildLoginRoute() {
+    return GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      path: '/login',
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          restorationId: 'route.login',
+          fullscreenDialog: true,
+          child: LoginScreen(
+            onLogin: () {
+              context.go('/home');
+            },
           ),
-          backButtonDispatcher: RootBackButtonDispatcher(),
-        ),
-      );
+        );
+      },
+    );
+  }
+
+  ShellRoute _buildHomeRoute() {
+    return ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        pageBuilder: (context, state, child) {
+          return MaterialPage(
+            restorationId: 'router.home',
+            child: RootScreen(
+              child: child,
+              onFabClick: () {
+                context.push('/home/creation');
+              },
+            ),
+          );
+        },
+        routes: [
+          GoRoute(
+              path: '/home',
+              pageBuilder: (context, state) {
+                return const MaterialPage(
+                  restorationId: 'route.home',
+                  child: ListScreen(),
+                );
+              },
+              routes: [
+                GoRoute(
+                  path: 'creation',
+                  pageBuilder: (context, state) {
+                    return MaterialPage(
+                      restorationId: 'route.home.creation',
+                      child: CreationScreen(
+                        onCreate: () {
+                          context.pop('/home');
+                        },
+                      ),
+                    );
+                  },
+                )
+              ]),
+        ]);
+  }
 }
