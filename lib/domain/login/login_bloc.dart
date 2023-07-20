6c7f6de8c4c28add59ab376d7bcd0e9ad60b22d3
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-20 19:33:44 +0300

    update LoginBloc

--- lib/domain/login/login_bloc.dart
+++ lib/domain/login/login_bloc.dart
@@ -1,18 +1,20 @@
-import 'package:bloc_app/data/fake_repository.dart';
+import 'package:bloc_app/data/auth_repository.dart';
+import 'package:bloc_app/data/entity/remote_user.dart';
 import 'package:bloc_app/domain/login/event.dart';
 import 'package:bloc_app/domain/login/state.dart';
 import 'package:bloc_app/ui/theme/resources/strings.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginBloc extends Bloc<Event, LoginState> {
-  final repository = FakeRepository();
+  final repository = AuthRepository();
 
   LoginBloc() : super(LoginState()) {
     on<LoginEvent>((event, emit) async {
       emit(LoginState(isLoading: true));
 
       try {
-        bool isAuthenticated = await repository.authenticate();
+        RemoteUser remoteUser = await repository.authenticate(event.email);
+        bool isAuthenticated = true;
 
         emit(LoginState(
           isAuthenticated: isAuthenticated,
