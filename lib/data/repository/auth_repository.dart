18c47bd20dfb07739ef06296558e031be850356b
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-21 20:00:17 +0300

    update auth datastore

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,10 +1,10 @@
 import 'package:bloc_app/data/entity/remote_user.dart';
-import 'package:bloc_app/data/storage/remote_datastore.dart';
+import 'package:bloc_app/data/storage/remote_user_datastore.dart';
 import 'package:bloc_app/service_locator.dart';
 import 'package:multiple_result/multiple_result.dart';
 
 class AuthRepository {
-  final datastore = locator<RemoteDataStore>();
+  final datastore = locator<RemoteUserDataStore>();
 
   Future<Result<RemoteUser, Exception>> authenticate(String email) async {
     return datastore.authenticate(email);
--- lib/data/storage/remote_user_datastore.dart
+++ lib/data/storage/remote_user_datastore.dart
@@ -3,7 +3,7 @@ import 'package:bloc_app/data/entity/remote_user.dart';
 import 'package:bloc_app/service_locator.dart';
 import 'package:multiple_result/multiple_result.dart';
 
-class RemoteDataStore {
+class RemoteUserDataStore {
   final api = locator<AuthApi>();
 
   Future<Result<RemoteUser, Exception>> authenticate(String email) async {
