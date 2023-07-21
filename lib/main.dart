f2a69314f4e65c94d9fc2b2f93691d319975ef17
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-21 18:31:41 +0300

    setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:bloc_app/service_locator.dart';
 import 'package:flutter/material.dart';
 
 import 'application.dart';
 
-void main() => runApp(Application());
+void main() {
+  configureDependencies();
+  runApp(Application());
+}
