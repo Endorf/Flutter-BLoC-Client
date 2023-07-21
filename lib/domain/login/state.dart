fbf9060f8385731bc54b161fd076926948ea4f75
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-21 17:18:17 +0300

    implement Copyable

--- lib/domain/login/state.dart
+++ lib/domain/login/state.dart
@@ -1,13 +1,34 @@
-class LoginState {
+import 'package:bloc_app/data/entity/remote_user.dart';
+import 'package:bloc_app/util/%20copy.dart';
+
+class LoginState implements Copyable<LoginState> {
   final bool isAuthenticated;
+  final RemoteUser? user;
   final bool isLoading;
   final bool hasError;
   final String? message;
 
   LoginState({
     this.isAuthenticated = false,
+    this.user,
     this.isLoading = false,
     this.hasError = false,
     this.message,
   });
+
+  @override
+  LoginState copyWith({
+    bool? isAuthenticated,
+    RemoteUser? user,
+    bool? isLoading,
+    bool? hasError,
+    String? message,
+  }) {
+    return LoginState(
+        isAuthenticated: isAuthenticated ?? this.isAuthenticated,
+        user: user ?? this.user,
+        isLoading: isLoading ?? this.isLoading,
+        hasError: hasError ?? this.hasError,
+        message: message ?? this.message);
+  }
 }
