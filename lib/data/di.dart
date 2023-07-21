04ca217a64efa94d60654909a6b764154f5d97a0
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-07-21 20:01:25 +0300

    update di

--- lib/data/di.dart
+++ lib/data/di.dart
@@ -1,23 +1,35 @@
 import 'package:bloc_app/data/api/auth_api.dart';
+import 'package:bloc_app/data/api/notes_api.dart';
 import 'package:bloc_app/data/repository/auth_repository.dart';
 import 'package:bloc_app/data/repository/fake_repository.dart';
-import 'package:bloc_app/data/storage/remote_datastore.dart';
+import 'package:bloc_app/data/repository/notes_repository.dart';
+import 'package:bloc_app/data/storage/remote_note_datastore.dart';
+import 'package:bloc_app/data/storage/remote_user_datastore.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 @module
 abstract class DataModule {
-  @singleton
-  Dio dio() => Dio();
-
+  // API
   @singleton
   AuthApi get authApi => AuthApi();
 
   @singleton
-  RemoteDataStore get remoteDataStore => RemoteDataStore();
+  NotesApi get notesApi => NotesApi();
 
+  // Data store
+  @singleton
+  RemoteUserDataStore get remoteUserDataStore => RemoteUserDataStore();
+
+  @singleton
+  RemoteNoteDataStore get remoteNoteDataStore => RemoteNoteDataStore();
+
+  // Repository
   @injectable
   AuthRepository get authRepository => AuthRepository();
+
+  @injectable
+  NoteRepository get noteRepository => NoteRepository();
 
   @injectable
   FakeRepository get fakeRepository => FakeRepository();
